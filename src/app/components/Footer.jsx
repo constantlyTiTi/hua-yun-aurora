1501731ca98ae3bd287c99e651ddf0bfcaf5a15b
ting <tli101@my.centennialcollege.ca>
2024-07-16 00:05:48 -0400

    fix:remove unused items

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="w-dvw p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://huayunaurora.ca/" className="hover:underline">HUA-YUN-AURORA</a>. All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">About</a>
                     </li>
@@ -20,7 +20,7 @@ const Footer = () => {
                     <li>
                         <a href="#" className="hover:underline">Contact</a>
                     </li>
-                </ul>
+                </ul> */}
             </div>
         </footer>
     )
