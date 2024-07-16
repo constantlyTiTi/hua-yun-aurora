0a838d953a6028229aab49bd99317e616f53d8cd
ting <tli101@my.centennialcollege.ca>
2024-07-16 13:46:56 -0400

    fix:show the hidden part of the img

--- src/app/components/ActivityDetails.jsx
+++ src/app/components/ActivityDetails.jsx
@@ -34,14 +34,14 @@ const ActivityDetails = ({ title, dateTime, description, picsUrls, videoUrls }) 
 
     return (
         setting && <div className="text-left grid gap-3 p-3">
-            <div className={`bg-black flex flex-col w-full h-svh z-50 top-0 left-0 fixed  ${isScaleOpen ? "block" : "hidden"}`}>
+            <div className={`bg-black flex flex-col w-full h-svh overflow-auto z-50 top-0 left-0 fixed  ${isScaleOpen ? "block" : "hidden"}`}>
                 <div className="flex justify-end p-2 group">
                     <svg onClick={closeScaleImage} className="rounded-lg h-8 w-8 text-red-500 hover:bg-gray-100 dark:hover:bg-gray-700" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
 
                 </div>
-                <div className="h-5/6 bg-white w-auto m-auto">
-                    {scaleImageUrl && <img className="h-full w-auto rounded-lg" src={scaleImageUrl} alt="activity picture" />}
-                </div>
+                {/* <div className="h-5/6 bg-white w-5/6 m-auto"> */}
+                    {scaleImageUrl && <img className="h-auto w-auto p-3 rounded-lg m-auto" src={scaleImageUrl} alt="activity picture" />}
+                {/* </div> */}
             </div>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
             <Accordion id="activityDetails" variant="splitted" selectionMode="multiple" defaultExpandedKeys={["1", "2", "3", "4"]} itemClasses={itemClasses}>
