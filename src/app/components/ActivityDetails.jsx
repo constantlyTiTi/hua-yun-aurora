b5de9ccfa62bafa80dec02df8ef0b1916c9e2f5a
ting <tli101@my.centennialcollege.ca>
2024-07-16 14:37:56 -0400

    fix:video cover not showing up

--- src/app/components/ActivityDetails.jsx
+++ src/app/components/ActivityDetails.jsx
@@ -68,8 +68,8 @@ const ActivityDetails = ({ title, dateTime, description, picsUrls, videoUrls }) 
                             {
                                 videoUrls.map((url, index) =>
 
-                                    <video key={`video-${index}`} preload="metadata" className="w-full h-full" controls>
-                                        <source key={`source-${index}`} src={url} type="video/mp4" />
+                                    <video key={`video-${index}`} className="w-full h-full" controls>
+                                        <source key={`source-${index}#t=0.001`} src={url} type="video/mp4" />
                                         <img src={url} title="Your browser does not support the <video> tag" />
                                     </video>
                                 )
