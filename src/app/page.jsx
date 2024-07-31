b36ddecb1f98d673d5b0a3ed5575916a5c1df13f
ting <tli101@my.centennialcollege.ca>
2024-07-31 15:19:45 -0400

    feature/disable cache of home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
 
 import Introduction from "./components/Introduction";
+import { revalidatePath } from 'next/cache'
 
 export default function Home() {
+
+  revalidatePath('/')
 
   return (
     <Introduction/>
