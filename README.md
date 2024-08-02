14ac2e930b17e02c65f58235bcdf472be259c567
ting <tli101@my.centennialcollege.ca>
2024-08-02 14:32:36 -0400

    fix:fix format

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-### Introduction 
+### Introduction
+
 This project is built for Hua-Yun-Aurora, which is a non-profit organization located in Aurora.
 
 The project is build with Next.js and all the data are saved in Contentful and AWS S3.
 
-please visit  [HUA YUN AURORA](https://www.huayunaurora.ca) for details information
+please visit [HUA YUN AURORA](https://www.huayunaurora.ca) for details information
--- src/app/components/SidebarItem.jsx
+++ src/app/components/SidebarItem.jsx
@@ -5,7 +5,7 @@ const SidebarItem = ({ teamNameShort, teamNameFull, onclick, sysId }) => {
     <div
       key={`sidebarItem-${sysId}`}
       onClick={() => onclick(teamNameShort)}
-      className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+      className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
     >
       <span key={`sidebarItemSpan-${sysId}`} className="ms-3">
         {teamNameFull}
