01da550e31820b949035650c8042adfa652ff796
ting <tli101@my.centennialcollege.ca>
2024-08-05 22:06:03 -0400

    remove comments

--- src/app/components/ActivityDetails.jsx
+++ src/app/components/ActivityDetails.jsx
@@ -48,36 +48,6 @@ const ActivityDetails = ({
           images={picsUrls}
           closeScaleImage={closeScaleImage}
         />
-        {/* <div
-          className={`fixed left-0 top-0 z-50 flex h-svh w-full flex-col overflow-auto bg-black ${isScaleOpen ? "block" : "hidden"}`}
-        >
-          <div className="group flex justify-end p-2">
-            <svg
-              onClick={closeScaleImage}
-              className="h-8 w-8 rounded-lg text-red-500 hover:bg-gray-100 dark:hover:bg-gray-700"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              {" "}
-              <path stroke="none" d="M0 0h24v24H0z" />{" "}
-              <line x1="18" y1="6" x2="6" y2="18" />{" "}
-              <line x1="6" y1="6" x2="18" y2="18" />
-            </svg>
-          </div>
-          {scaleImageUrl && (
-            <img
-              className="m-auto h-auto w-auto rounded-lg p-3"
-              src={scaleImageUrl}
-              alt="activity picture"
-            />
-          )}
-        </div> */}
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
