e19c86cbb386e06f636f364ef890ad95662d99ab
ting <tli101@my.centennialcollege.ca>
2024-08-09 13:37:35 -0400

    fix/make button text to be customized

--- src/app/components/Introduction.jsx
+++ src/app/components/Introduction.jsx
@@ -14,7 +14,7 @@ const Introduction = async () => {
     introduction && (
       <div className="md:flex-cols-2 flex w-full content-start gap-5">
         <div className="flex-none">
-          {introduction?.introductionImageUrl && (
+          {introduction.introductionImageUrl && (
             <img
               className="h-auto max-w-full rounded-lg"
               src={introduction.introductionImageUrl}
@@ -27,9 +27,9 @@ const Introduction = async () => {
           <Link
             type="button"
             href="/teams"
-            className="mb-2 me-2 mt-5 inline-block w-20 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mb-2 me-2 mt-5 inline-block w-auto rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Details
+            {introduction.buttonText}
           </Link>
         </div>
       </div>
